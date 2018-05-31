4b6c00e75d4007ae34468e5cd8dce7eb43d0f228
Elihofni Lima <elihofni.lima@globoesporte.com>
2018-05-30 22:58:08 -0300

    verificacao E e OU

--- teste1.hs
+++ teste1.hs
@@ -1,47 +1,25 @@
-{- 
+ {-
     Linguagens de Programaçao - Prof Bruno
 
     ROTEIRO
     1- Instale haskell
-    2- Entre no terminal (ghci)
-    3- Chame `funcaoPrincipal`
+    2- Entre no terminal 
+      ghci
+      :set prompt "ghci "
+    3- Chame verificaValidade com os parâmetros necessários
 
     Participantes
     - Allana
     - Eli 
     - Gabriel
 
--- Essa é um exemplo de entrada do nosso programa, mude se quiser
-let frame = [("alfa" || "beta"), "beta", ["teta"], not("gama")]
-
-let grafo = [('a', 'b', "alfa"), ('b', 'c', "beta"), ('c', 'd', "teta"), ('d', 'e', "gama")]
-
-let pdl = "(alfa U beta) E gama;"
-let pdl = "(<alfa U beta>; teta*); ¬(gama) -> <->"
-let grafoEntrada = "(a, b, alfa), (b, c, beta), (c, d, teta)"
-
--- conversaoDeEntradaPDLparaPrograma = pdl -> grafoEntrada -> codigoBonito
-
--- fruncaoPrincipal = frame -> grafo -> True || False ('d', 'e', "gama")
-
--- import Data.List.Utils (replace)
--- replace "o" "x" "Hello World"
+    Esse são exemplos de entrada do nosso programa
+     [("alfa")] []
+     [("alfa")] [("A", "B", "alfa")]
+     [("alfa E beta")] [("A", "B", "alfa")]
+     [("alfa OU beta")] [("A", "B", "outro")]
 -}
 
--- Substitui todas as ocorrências
-replace :: String -> (String -> (String -> String))
-
-replace [] old new = []
-replace str old new = loop str
-  where
-    loop [] = []
-    loop str =
-      let (prefix, rest) = splitAt n str
-      in
-        if old == prefix                
-        then new ++ loop rest           
-        else head str : loop (tail str)
-    n = length old
 
 -- Retorna cada elemento da tripla
 priVar :: (a, b, c) -> a
@@ -53,20 +31,34 @@ ope (_, b, _) = b
 segVar :: (a, b, c) -> c
 segVar (_, _, c) = c
 
--- TODO continuar 
--- Frame: Lista de comandos PDL definidos por triplas (variavel, comando, variavel)
+-- Frame: Lista de comandos PDL definidos por string ("variavel1 operador variavel2")
 -- Grafo: Classe de um grafo (verticeOrigem, verticeDestino, programa)
-verificaValidade :: [(String, String, String)] -> ([(String, String, String)] -> (Bool, String))
+verificaValidade :: [(String)] -> ([(String, String, String)] -> (Bool, String))
 verificaValidade [] [] = (True, "Eh valido :)")
 verificaValidade [] grafo = (False, "PDL inválida")
 verificaValidade frame grafo = (status, texto)
   where
-    primeiraVar = priVar (head frame)
-    operador = ope (head frame)
-    segundaVar = segVar (head frame)
-    status = if operador == ";" then True else False
-    texto = if status then "OK" else "Erro em " ++ segVar (head grafo)
-    -- if status then verificaValidade (tail frame) (tail grafo) else (False, "Deu ruim ai tio")
+    partes = words (head frame)
+    primeiraVar = head partes
+    operador = if length partes == 3 then partes !! 1 else ""
+    segundaVar = last partes
+    status = if not (null grafo)
+             then verificaStatus (primeiraVar, operador, segundaVar) (segVar (head grafo))
+             else True
+    texto = if status
+            then "PDL valida"
+            else "Erro em (" ++ priVar (head grafo) ++ ", " ++ ope (head grafo) ++ ", " ++ segVar (head grafo) ++ ")"
+
+-- Comando: (variavel1, operador, variavel2)
+-- Programa: "alfa"
+verificaStatus :: (String, String, String) -> String -> Bool
+verificaStatus (var1, op, var2) programa
+  | op == "OU"  = programa == var1 || programa == var2
+  | op == "E"   = programa == var1 || programa == var2
+  | null op     = programa == var1
+  | otherwise   = False
+
+
 
 
 
